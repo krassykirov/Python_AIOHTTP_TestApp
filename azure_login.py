12e9a21538244da1a6386b3b45424752f1c0a03d
unknown <krassy.kirov@gmail.com>
2020-11-25 19:25:08 +0200

    jquery

--- azure_login.py
+++ azure_login.py
@@ -6,35 +6,17 @@ import aiohttp_jinja2
 from aiohttp import web
 from msrestazure.azure_active_directory import AADTokenCredentials
 from aiohttp_session import get_session
+from helper_db import get_app_config_data_from_key_vault
 import pyodbc
 
-def get_app_config_data_from_key_vault():
-    msi_endpoint = os.environ.get("MSI_ENDPOINT")
-    msi_secret = os.environ.get("MSI_SECRET")
-    token_auth_uri = f"{msi_endpoint}?resource=https://vault.azure.net&api-version=2017-09-01"
-    head_msi = {'Secret': msi_secret}
-    resp = requests.get(token_auth_uri, headers=head_msi)
-    access_token = resp.json()['access_token']
-    con_str = "https://krassykeyvault.vault.azure.net/secrets/constr?api-version=2016-10-01"
-    app_config = "https://krassykeyvault.vault.azure.net/secrets/kr9data?api-version=2016-10-01"
-    headers = {'Authorization': 'Bearer {}'.format(access_token)}
-    constr = requests.get(con_str, headers=headers).json()
-    app_data = requests.get(app_config, headers=headers).json()
-    constr = constr.get('value')  # str_to_dict => d1 = eval('string_value')
-    app_data = app_data.get('value')
-    app_data = json.loads(app_data)
-    return constr, app_data
 
 conn_str,data = get_app_config_data_from_key_vault()
 
 async def db_connect():
     try:
         con = pyodbc.connect(conn_str)
-        if con:
-            return con
-        else:
-            raise
-    except Exception as e:
+        return con
+    except pyodbc.Error as e:
         print(e)
 
 CLIENT_ID = data['CLIENT_ID']
--- helper_db.py
+++ helper_db.py
@@ -1,21 +1,24 @@
-# import pyodbc,requests
-# import os,json
-#
-# async def db_connect():
-#     conn_str = get_connstr_from_key_vault()
-#     con = pyodbc.connect(conn_str)
-#     return con
+import pyodbc,requests
+import os,json
 
-# def get_connstr_from_key_vault():
-#     msi_endpoint = os.environ.get("MSI_ENDPOINT")
-#     msi_secret = os.environ.get("MSI_SECRET")
-#     token_auth_uri = f"{msi_endpoint}?resource=https://vault.azure.net&api-version=2017-09-01"
-#     head_msi = {'Secret': msi_secret}
-#     resp = requests.get(token_auth_uri, headers=head_msi)
-#     access_token = resp.json()['access_token']
-#     endpoint = "https://krassykeyvault.vault.azure.net/secrets/constr?api-version=2016-10-01"
-#     headers = {"Authorization": 'Bearer {}'.format(access_token)}
-#     response = requests.get(endpoint, headers=headers).json()
-#     conn_str = response.get('value')
-#     return conn_str
+async def db_connect():
+    conn_str = get_app_config_data_from_key_vault()
+    con = pyodbc.connect(conn_str)
+    return con
 
+def get_app_config_data_from_key_vault():
+    msi_endpoint = os.environ.get("MSI_ENDPOINT")
+    msi_secret = os.environ.get("MSI_SECRET")
+    token_auth_uri = f"{msi_endpoint}?resource=https://vault.azure.net&api-version=2017-09-01"
+    head_msi = {'Secret': msi_secret}
+    resp = requests.get(token_auth_uri, headers=head_msi)
+    access_token = resp.json()['access_token']
+    con_str = "https://krassykeyvault.vault.azure.net/secrets/constr?api-version=2016-10-01"
+    app_config = "https://krassykeyvault.vault.azure.net/secrets/kr9data?api-version=2016-10-01"
+    headers = {'Authorization': 'Bearer {}'.format(access_token)}
+    constr = requests.get(con_str, headers=headers).json()
+    app_data = requests.get(app_config, headers=headers).json()
+    constr = constr.get('value')  # str_to_dict => d1 = eval('string_value')
+    app_data = app_data.get('value')
+    app_data = json.loads(app_data)
+    return constr, app_data
